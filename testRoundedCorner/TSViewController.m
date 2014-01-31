cbaf788954531c6a5249c77dfcd436843ab8a9de
TomSwift <nicholashodapp@gmail.com>
2014-01-31 08:51:50 -0800

    added a second solution

--- testRoundedCorner/TSViewController.m
+++ testRoundedCorner/TSViewController.m
@@ -17,15 +17,71 @@
 {
     IBOutlet UIImageView*           _backgroundView;
     
+    IBOutlet UIView*                _masterContainerView;
+    
+    IBOutlet UIView*                _detailContainerView;
+    
+    
+    // solution 1:
+    
     IBOutlet TSRoundedCornerView*   _roundedCornerView;
+    
+    // solution 2:
+    
+    IBOutlet UIView*                _roundedCornerContainer;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
+    
+    [self.view addConstraint: [NSLayoutConstraint constraintWithItem: _masterContainerView
+                                                           attribute: NSLayoutAttributeBottom
+                                                           relatedBy: NSLayoutRelationEqual
+                                                              toItem: self.view
+                                                           attribute: NSLayoutAttributeBottom
+                                                          multiplier: 1
+                                                            constant: 0]];
+    
+    
+    [self.view addConstraint: [NSLayoutConstraint constraintWithItem: _detailContainerView
+                                                           attribute: NSLayoutAttributeBottom
+                                                           relatedBy: NSLayoutRelationEqual
+                                                              toItem: self.view
+                                                           attribute: NSLayoutAttributeBottom
+                                                          multiplier: 1
+                                                            constant: 0]];
+
+    [self.view addConstraint: [NSLayoutConstraint constraintWithItem: _detailContainerView
+                                                           attribute: NSLayoutAttributeRight
+                                                           relatedBy: NSLayoutRelationEqual
+                                                              toItem: self.view
+                                                           attribute: NSLayoutAttributeRight
+                                                          multiplier: 1
+                                                            constant: 0]];
+
+    
     _roundedCornerView.corner = UIRectCornerTopLeft;
     _roundedCornerView.backgroundView = _backgroundView;
+
+    [self solution1: nil];
+}
+
+- (IBAction) solution1: (id) sender
+{
+    _roundedCornerView.hidden = NO;
+    
+    _roundedCornerContainer.layer.cornerRadius = 0;
+    _roundedCornerContainer.layer.masksToBounds = NO;
+}
+
+- (IBAction) solution2: (id) sender
+{
+    _roundedCornerView.hidden = YES;
+    
+    _roundedCornerContainer.layer.cornerRadius = 15;
+    _roundedCornerContainer.layer.masksToBounds = YES;
 }
 
 @end
